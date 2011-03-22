e8c6f12620246b3125aed5e785707fbcaccf98be
pedxing <dejarp@rpi.edu>
2011-03-21 17:31:24 -0700

    push test

--- JiraPhone/Classes/AbstractConstant.h
+++ JiraPhone/Classes/AbstractConstant.h
@@ -5,6 +5,8 @@
 //  Created by Aleksey Maslov on 11/24/10.
 //  Copyright 2010 AMaslov. All rights reserved.
 //
+//test change
+//test chang2
 #import <Foundation/Foundation.h>
 #import "AbstractNamedEntity.h"
 
