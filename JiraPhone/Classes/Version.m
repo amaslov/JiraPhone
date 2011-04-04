6bc7178660dc4edcd786eb7ada66f04843deef4a
Aleksey Maslov <alexey5085@gmail.com>
2011-04-04 18:32:04 -0400

    Added version class

--- JiraPhone/Classes/Version.m
+++ JiraPhone/Classes/Version.m
@@ -15,6 +15,54 @@
 //@synthesize archived=_archived;
 //@synthesize released=_released;
 
+- (void) dealloc
+{
+	if(self.releaseDate != nil) { [self.releaseDate release]; }
+	if(self.sequence != nil) { [self.sequence release]; }
+	[super dealloc];
+}
 
+#pragma mark -
+#pragma mark Class methods
++ (void)cacheVersions:(NSArray *)_versions {
+	FMDatabase *db = [JiraPhoneAppDelegate sharedDB];	
+	
+	// clear
+	NSString *updateString = @"delete from versions";
+	[db executeUpdate:updateString];
+	
+	// insert
+	for (Version *ver in _versions) {
+		updateString = [NSString stringWithFormat:@"insert into versions (version_id, name, releaseDate, server, sequence) values (\"%@\", \"%@\", \"%@\", \"%@\", \"%@\")", 
+						ver.ID, ver.name, ver.releaseDate, [User loggedInUser].server, ver.sequence;
+		[db executeUpdate:updateString];		
+	}
+}
 
++ (void)getCachedVersions:(NSMutableArray *)_versions {
+	
+	NSString *queryString = [NSString stringWithFormat: @"select * from versions where server = \"%@\"", [User loggedInUser].server];
+	
+	FMDatabase *db = [JiraPhoneAppDelegate sharedDB];
+	FMResultSet *rs = [db executeQuery:queryString];
+	while ([rs next])
+	{
+		Version *version = [[Version alloc]init];
+		[version fillFromResultSet:rs];
+		[_versions addObject:version];
+		[version release];
+	}
+	[rs close];
+}
+
+#pragma mark -
+#pragma mark Private Methods
+- (void)fillFromResultSet:(FMResultSet *)rs
+{
+	self.ID=[rs stringForColumn:@"version_id"];
+	self.name=[rs stringForColumn:@"name"];
+	self.releaseDate=[rs stringForColumn:@"releaseDate"];
+	self.sequence=[rs stringForColumn:@"sequence"];
+	self.server=[User loggedInUser].server;
+}
 @end
